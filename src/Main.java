a4032734f0ceac009b947e6d8c9b1add8c06af33
Nestor-Florehs <nestor.flores@students.salle.url.edu>
2024-12-08 11:30:49 +0100

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import Presentation.Controller;
+
+import java.io.IOException;
+
+/**
+ * The Main class serves as the entry point for the program.
+ * It contains the main method which initializes the Controller class and starts its execution.
+ *
+ * @throws IOException if an input/output error occurs.
+ */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static void main(String[] args) throws IOException {
+        Controller controller = new Controller();
+        controller.run();
     }
 }
